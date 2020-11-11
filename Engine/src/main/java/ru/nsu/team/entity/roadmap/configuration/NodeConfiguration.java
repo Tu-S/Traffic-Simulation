25fa691b588ed56e5e31130c1091f97aa953344b
Sergey <tussss89@gmail.com>
2020-11-11 19:28:05 +0700

    roadsIn and roadsOut

--- Engine/src/main/java/ru/nsu/team/entity/roadmap/configuration/NodeConfiguration.java
+++ Engine/src/main/java/ru/nsu/team/entity/roadmap/configuration/NodeConfiguration.java
@@ -3,17 +3,17 @@ package ru.nsu.team.entity.roadmap.configuration;
 import java.util.List;
 
 public class NodeConfiguration {
-    private List<Integer> roadsTo;
-    private List<Integer> roadsFrom;
+    private List<Integer> roadsOut;
+    private List<Integer> roadsIn;
     private TrafficLightConfiguration trafficLight;
     private boolean isSpawner;
 
-    public List<Integer> getRoadsTo() {
-        return roadsTo;
+    public List<Integer> getRoadsOut() {
+        return roadsOut;
     }
 
-    public List<Integer> getRoadsFrom() {
-        return roadsFrom;
+    public List<Integer> getRoadsIn() {
+        return roadsIn;
     }
 
     public boolean isSpawner() {
