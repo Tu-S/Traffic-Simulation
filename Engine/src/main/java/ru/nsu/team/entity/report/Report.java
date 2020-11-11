9f822df0c4b0da849297945ab23f9bfd2cda7fa0
Jlemix <unuashechka@gmail.com>
2020-11-11 21:47:39 +0700

    heatmap class added, upd. for reporter, traffic statistics

--- Engine/src/main/java/ru/nsu/team/entity/report/Report.java
+++ Engine/src/main/java/ru/nsu/team/entity/report/Report.java
@@ -1,4 +1,23 @@
 package ru.nsu.team.entity.report;
 
+import ru.nsu.team.entity.statistics.RoadState;
+import ru.nsu.team.entity.statistics.TrafficStatistics;
+
+import java.util.List;
+
 public class Report {
+  private TrafficStatistics trafficStatistics;
+
+  public Report(TrafficStatistics trafficStatistics) {
+    this.trafficStatistics = trafficStatistics;
+  }
+
+  // make report from traffic statistics
+  public void makeReport() {
+    List<RoadState> roadStates = trafficStatistics.getRoadStatistics();
+    // prepare heatmap data for editor:
+    // ...
+    // HeatMap heatMap = new HeatMap(roadStates, begin, end);
+    // ...
+  }
 }
---
+++ Engine/src/main/java/ru/nsu/team/entity/statistics/HeatMap.java
@@ -0,0 +1,11 @@
+package ru.nsu.team.entity.statistics;
+
+import java.util.List;
+
+public class HeatMap {
+
+  public HeatMap(List<RoadState> roadStates, int intervalBegin, int intervalEnd) {
+    // for each road if time >= intervalBegin && time <= intervalEnd, calculate traffic congestion:
+    // ...
+  }
+}
