bf13cf84108a5e740290ef9e76f06fd633691176
SpykeX3 <spykex3@gmail.com>
2020-11-19 11:21:02 +0700

    added intersections for course

--- Engine/src/main/java/ru/nsu/team/entity/roadmap/Course.java
+++ Engine/src/main/java/ru/nsu/team/entity/roadmap/Course.java
@@ -1,17 +1,26 @@
 package ru.nsu.team.entity.roadmap;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Course {
 
-    private Road fromRoad;
-    private Road toRoad;
+    private final Road fromRoad;
+    private final Road toRoad;
     private int timeLeft;
     private int length;
+    private final List<Intersection> intersections;
 
     public Course(Road fromRoad, Road toRoad) {
         this.fromRoad = fromRoad;
         this.toRoad = toRoad;
+        intersections = new ArrayList<>();
+    }
+
+    public Course(Road fromRoad, Road toRoad, List<Intersection> intersections) {
+        this.fromRoad = fromRoad;
+        this.toRoad = toRoad;
+        this.intersections = intersections;
     }
 
     public Road getFromRoad() {
@@ -22,27 +31,27 @@ public class Course {
         return toRoad;
     }
 
-    public int getTimeLeft() {
-        return timeLeft;
-    }
-
     public int getLength() {
         return length;
     }
 
-    public void setFromRoads(Road fromRoads) {
-        this.fromRoad = fromRoads;
+    public void setLength(int length) {
+        this.length = length;
     }
 
-    public void setToRoad(Road toRoad) {
-        this.toRoad = toRoad;
+    public int getTimeLeft() {
+        return intersections.stream().map(Intersection::getTimeLeft).min(Integer::compareTo).orElse(0);
     }
 
     public void setTimeLeft(int timeLeft) {
-        this.timeLeft = timeLeft;
+        for (Intersection i : intersections) {
+            i.setTimeLeft(timeLeft);
+        }
     }
 
-    public void setLength(int length) {
-        this.length = length;
+    public void decreaseTime(int timeUsed) {
+        for (Intersection i : intersections) {
+            i.setTimeLeft(i.getTimeLeft() - timeUsed);
+        }
     }
 }
