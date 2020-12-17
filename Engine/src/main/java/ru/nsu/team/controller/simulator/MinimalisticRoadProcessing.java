c5832d07a49911f8a8c42aa159b0994e28f9008d
SpykeX3 <spykex3@gmail.com>
2020-12-17 13:07:02 +0700

    Added utility methods for RoadProcessing

--- Engine/src/main/java/ru/nsu/team/controller/simulator/MinimalisticRoadProcessing.java
+++ Engine/src/main/java/ru/nsu/team/controller/simulator/MinimalisticRoadProcessing.java
@@ -2,9 +2,12 @@ package ru.nsu.team.controller.simulator;
 
 import ru.nsu.team.entity.roadmap.Node;
 import ru.nsu.team.entity.roadmap.Road;
+import ru.nsu.team.entity.trafficparticipant.Car;
+import ru.nsu.team.entity.trafficparticipant.PositionOnRoad;
 import ru.nsu.team.entity.trafficparticipant.TrafficParticipant;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CountDownLatch;
@@ -14,12 +17,14 @@ public class MinimalisticRoadProcessing implements Runnable {
     private final Road targetRoad;
     private final CountDownLatch latch;
     private final int timeFrameStart;
+    private final HashMap<Integer, TrafficParticipant> blockingVehicles;
 
     public MinimalisticRoadProcessing(int timeFrameStart, Road targetRoad, Set<Node> activeNodes, CountDownLatch latch) {
         this.activeNodes = activeNodes;
         this.targetRoad = targetRoad;
         this.latch = latch;
         this.timeFrameStart = timeFrameStart;
+        this.blockingVehicles = new HashMap<>();
     }
 
     private boolean checkLaneChange(TrafficParticipant car) {
@@ -51,8 +56,68 @@ public class MinimalisticRoadProcessing implements Runnable {
         saveCarState(car, timeFrameStart + timePassed);
     }
 
-    private void moveCarStraight(TrafficParticipant car) {
+    private void moveCarStraight(TrafficParticipant participant) {
+        PositionOnRoad position = participant.getPosition();
+        Car car = participant.getCar();
+        double distance;
+        double acceleration = Car.DEFAULT_ACCELERATION;
+        int timeOfAcceleration = Math.min((int) ((car.getMaxSpeed() - car.getSpeed()) / Car.DEFAULT_ACCELERATION), car.getTimeLeft());
+        double distanceOfAcceleration = acceleration * timeOfAcceleration * timeOfAcceleration / 2 + car.getSpeed() * timeOfAcceleration;
 
+        if (blockingVehicles.containsKey(participant.getPosition().getCurrentLane())) {
+            // Node is blocked by another car
+            TrafficParticipant block = blockingVehicles.get(participant.getPosition().getCurrentLane());
+            distance = participant.getPosition().getPosition() - block.getPosition().getPosition();
+            double possibleDistance = Math.min(distanceOfAcceleration * (car.getTimeLeft() - timeOfAcceleration), distance);
+
+            if (car.getTimeLeft() >= timeOfAcceleration) {
+                if (distanceOfAcceleration > distance) {
+                    int timeAccelerating = (int) (distance / (acceleration * acceleration / 2 + car.getSpeed()));
+
+                } else {
+                    distance = Math.min(distance, possibleDistance);
+                }
+            }
+
+
+        } else {
+            // Node is not blocked by another car
+            double possibleDistance = distanceOfAcceleration * (car.getTimeLeft() - timeOfAcceleration);
+            if (possibleDistance >= position.getPosition()) {
+                // Car can reach the node
+
+
+            } else {
+                // Car can't reach the node
+            }
+        }
+    }
+
+    private double calculateDistanceByTime(TrafficParticipant participant, int time) {
+        Car car = participant.getCar();
+        double acceleration = car.getAcceleration();
+        int timeOfAcceleration = (int) ((car.getMaxSpeed() - car.getSpeed()) / Car.DEFAULT_ACCELERATION);
+        double distanceOfAcceleration = acceleration * timeOfAcceleration * timeOfAcceleration / 2 + car.getSpeed() * timeOfAcceleration;
+        if (timeOfAcceleration > time) {
+            return acceleration * acceleration / 2 * time + car.getSpeed() * time;
+        }
+        return distanceOfAcceleration + car.getMaxSpeed() * (time - timeOfAcceleration);
+    }
+
+    private int calculateTimeByDistance(TrafficParticipant participant, double distance) {
+        Car car = participant.getCar();
+        double acceleration = car.getAcceleration();
+        int timeOfAcceleration = (int) ((car.getMaxSpeed() - car.getSpeed()) / Car.DEFAULT_ACCELERATION);
+        double distanceOfAcceleration = acceleration * timeOfAcceleration * timeOfAcceleration / 2 + car.getSpeed() * timeOfAcceleration;
+        if (distanceOfAcceleration > distance) {
+            return solveTimeSqEq(acceleration, car.getSpeed(), distance);
+        }
+        return timeOfAcceleration + (int) ((distance - distanceOfAcceleration) / car.getMaxSpeed());
+    }
+
+    private int solveTimeSqEq(double acceleration, double speed, double distance) {
+        double d = Math.sqrt(speed * speed + 4 * speed * distance);
+        return (int) ((-speed + d) / (2 * acceleration));
     }
 
 
